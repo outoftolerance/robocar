d22f9760b19bd76d6512c6039998eb9447af369d
James Howard <james@outoftolerance.com>
2017-08-27 00:59:30 -0700

    Seperated things out, added subtractions to improve yellow and white detection, still have reflection noise, need filtering

--- robocar.cpp
+++ robocar.cpp
@@ -11,10 +11,10 @@ int main (void)
 {
 	Capture capture;
 	//capture.init(0, 1920, 1080);
-	capture.init("../video3.mp4");
+	capture.init("../video4.mp4");
+	cv::VideoWriter output("../output.avi", CV_FOURCC('M','J','P','G'), 30, cv::Size(1920, 1080 - 400), true);
 
-	cv::Mat frame, frame_hsv, yellow_edges, white_edges, yellow_edges_color, white_edges_color, whites, yellows, epic;
-	cv::Mat input_view, input_hsv_view, input_blur_view, edges_view, lines_view, whites_view, yellows_view, epic_view;
+	cv::Mat frame, frame_hsv, yellow_edges, white_edges, yellow_edges_color, white_edges_color, whites, yellows, dulls, blacks, epic;
 	cv::vector<cv::Vec4i> yellow_lines, white_lines;
 
 	//set dilation element for edge gap filling
@@ -39,59 +39,82 @@ int main (void)
 
 		//cut out whites and yellows
 		cv::inRange(frame, cv::Scalar(180, 180, 180), cv::Scalar(255, 255, 255), whites);
+		cv::inRange(frame, cv::Scalar(0, 0, 0), cv::Scalar(180, 180, 180), blacks);
 		cv::inRange(frame_hsv, cv::Scalar(25, 50, 50), cv::Scalar(40, 255, 255), yellows);
 
-		//yellows
+		//if there is stuff in yellow, remove it from white
+		whites = whites & (255 - yellows);
+
+		//if there is stuff in black, remove it from yellow
+		yellows = yellows & (255 - blacks);
+
+		//yellows line detect
 		cv::blur(yellows, yellows, cv::Size(3,3));
 		cv::Canny(yellows, yellow_edges, 0, 250);
 		cv::dilate(yellow_edges, yellow_edges, element);
 		cv::cvtColor(yellow_edges, yellow_edges_color, CV_GRAY2BGR);
 
-		cv::HoughLinesP(yellow_edges, yellow_lines, 5, CV_PI/180, 100, 50, 150);
+		cv::HoughLinesP(yellow_edges, yellow_lines, 5, CV_PI/180, 100, 50, 300);
 
 		for(size_t i = 0; i < yellow_lines.size(); i++)
 		{
 			cv::line(yellow_edges_color, cv::Point(yellow_lines[i][0], yellow_lines[i][1]),
-				cv::Point(yellow_lines[i][2], yellow_lines[i][3]), cv::Scalar(0, 0, 255), 3, 8);
+				cv::Point(yellow_lines[i][2], yellow_lines[i][3]), cv::Scalar(0, 255, 255), 3, 8);
 		}
 
-		//whites
+		//whites line detect
 		cv::blur(whites, whites, cv::Size(3,3));
 		cv::Canny(whites, white_edges, 0, 250);
 		cv::dilate(white_edges, white_edges, element);
 		cv::cvtColor(white_edges, white_edges_color, CV_GRAY2BGR);
 
-		cv::HoughLinesP(white_edges, white_lines, 5, CV_PI/180, 300, 100, 25);
+		cv::HoughLinesP(white_edges, white_lines, 5, CV_PI/180, 300, 25, 25);
 
 		for(size_t i = 0; i < white_lines.size(); i++)
 		{
 			cv::line(white_edges_color, cv::Point(white_lines[i][0], white_lines[i][1]),
-				cv::Point(white_lines[i][2], white_lines[i][3]), cv::Scalar(0, 0, 255), 3, 8);
+				cv::Point(white_lines[i][2], white_lines[i][3]), cv::Scalar(0, 255, 0), 3, 8);
 		}
 
 		//combine the line outputs
 		epic = yellow_edges_color | white_edges_color;
 
+		output.write(epic);
+
 		//viewing windows
-		cv::resize(frame, input_view, cv::Size(), 0.25, 0.25);
-		cv::imshow("Input View", input_view);
-		cv::moveWindow("Input View", 0, 0);
+		cv::resize(frame, frame, cv::Size(), 0.25, 0.25);
+		cv::imshow("Input View", frame);
+		cv::moveWindow("Input View", 100, 100);
 
-		cv::resize(frame_hsv, input_hsv_view, cv::Size(), 0.25, 0.25);
-		cv::imshow("Input HSV View", input_hsv_view);
-		cv::moveWindow("Input HSV View", 0, frame.rows/3);
+		cv::resize(frame_hsv, frame_hsv, cv::Size(), 0.25, 0.25);
+		cv::imshow("Input HSV View", frame_hsv);
+		cv::moveWindow("Input HSV View", 100, 300);
 
-		cv::resize(whites, whites_view, cv::Size(), 0.25, 0.25);
-		cv::imshow("White Threshold View", whites_view);
-		cv::moveWindow("White Threshold View", 500 + 60, 0);
+		//Thresholds
+		cv::resize(whites, whites, cv::Size(), 0.25, 0.25);
+		cv::imshow("White Threshold View", whites);
+		cv::moveWindow("White Threshold View", 100 + 500, 100);
 
-		cv::resize(yellows, yellows_view, cv::Size(), 0.25, 0.25);
-		cv::imshow("Yellow Threshold View", yellows_view);
-		cv::moveWindow("Yellow Threshold View", 500 + 60, frame.rows/3);
+		cv::resize(yellows, yellows, cv::Size(), 0.25, 0.25);
+		cv::imshow("Yellow Threshold View", yellows);
+		cv::moveWindow("Yellow Threshold View", 100 + 500, 100 + 200);
 
-		cv::resize(epic, epic_view, cv::Size(), 0.25, 0.25);
-		cv::imshow("Combined Frame View", epic_view);
-		cv::moveWindow("Combined Frame View", 500 + 60, frame.rows*2/3);
+		cv::resize(blacks, blacks, cv::Size(), 0.25, 0.25);
+		cv::imshow("Black Threshold View", blacks);
+		cv::moveWindow("Black Threshold View", 100 + 500, 100 + 400);
+
+		//Lines
+		cv::resize(white_edges_color, white_edges_color, cv::Size(), 0.25, 0.25);
+		cv::imshow("White Lines View", white_edges_color);
+		cv::moveWindow("White Lines View", 100 + 500 * 2, 100);
+
+		cv::resize(yellow_edges_color, yellow_edges_color, cv::Size(), 0.25, 0.25);
+		cv::imshow("Yellow Lines View", yellow_edges_color);
+		cv::moveWindow("Yellow Lines View", 100 + 500 * 2, 100 + 200);
+
+		cv::resize(epic, epic, cv::Size(), 0.25, 0.25);
+		cv::imshow("Combined Lines View", epic);
+		cv::moveWindow("Combined Lines View", 100 + 500 * 2, 100 + 400);
 
         if(cvWaitKey(10) == 27)
             break;
